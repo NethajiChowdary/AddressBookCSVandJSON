6232c4536fea18497e555a35fe7acbf53669d90c
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-23 22:49:14 +0530

    Initial Commit: Welcome to AddressBook CSV and JSON File

---
+++ lib/src/main/java/addressbookcsvandjson/AddressBookMain.java
@@ -0,0 +1,10 @@
+package addressbookcsvandjson;
+
+public class AddressBookMain {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Address Book CSV and JSON File");
+	}
+
+}
